2db77b6dccca70c475dc7c6f59fd1d77ae46128e
shiMac <beau.shirdavani@bellevuecollege.edu>
2018-11-16 12:21:26 -0800

    made 2 Model classes
    Question class
    
    Question Bank class - holds list of questions

---
+++ Quizzler/Question.swift
@@ -0,0 +1,25 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by shiMac on 11/16/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    let questionText : String
+    let answer : Bool
+//    constructor - initializer
+    init(text : String, correctAnswer : Bool){
+        questionText = text
+        answer = correctAnswer
+    }
+//    method
+    
+    
+}
+
+
+
